82f28975d52403cec69c049cb8b3531ab66e3cac
88joonyc <joonyc88@gmail.com>
2021-07-20 16:17:19 -0400

    half way

---
+++ frontend/src/pages/ImagePage/ImagePage.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { useHistory } from 'react-router-dom';
+import { createAlbum } from '../../store/images';
+
+// import './AddImage.css'
+
+export default function AddImage () {
+
+
+}
