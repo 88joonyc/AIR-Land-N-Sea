5c51706930f355326da434dfd2da44776c969204
88joonyc <joonyc88@gmail.com>
2021-09-23 15:39:24 -0400

    signup

--- frontend/src/components/SignUpFormPage/index.js
+++ frontend/src/components/SignUpFormPage/index.js
@@ -31,108 +31,110 @@ export default function SignUpFormPage() {
     };
 
     return (
-        <form
-            onSubmit={handleSubmit}
-        >
-            <div className='container'>
-                <div className='form-container'>
-                    <div className='email-conmtainer container-lot'>
-                        <div className='email-label'>
-                            <label className='email-label'> First Name
-                                <div className='input-lot'>
-                                    <input
-                                        type='text'
-                                        value={firstName}
-                                        onChange={(e) => setFirstName(e.target.value)}
-                                        required
-                                        className='input-area'
-                                    />
+        <div className='signup-body'>
+            <form
+                onSubmit={handleSubmit}
+            >
+                <div className='signup-container'>
+                    <div className='form-container'>
+                        <div className='email-conmtainer container-lot'>
+                            <div className='email-label'>
+                                <label className='email-label'> First Name
+                                    <div className='input-lot'>
+                                        <input
+                                            type='text'
+                                            value={firstName}
+                                            onChange={(e) => setFirstName(e.target.value)}
+                                            required
+                                            className='input-area'
+                                        />
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div className='email-conmtainer container-lot'>
+                            <div className='email-label'>
+                                <label className='email-label'> Last Name
+                                    <div className='input-lot'>
+                                        <input
+                                            type='text'
+                                            value={lastName}
+                                            onChange={(e) => setLastName(e.target.value)}
+                                            required
+                                            className='input-area'
+                                        />
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div className='email-conmtainer container-lot'>
+                            <div className='email-label'>
+                                <label className='email-label'> Email
+                                    <div className='input-lot'>
+                                        <input
+                                            type='email'
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            required
+                                            className='input-area'
+                                        />
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div className='username-container container-lot'>
+                            <div className='username-label'>
+                                <label className='email-label'> Username
+                                    <div className='input-lot'>
+                                        <input
+                                            type='text'
+                                            value={username}
+                                            onChange={(e) => setUsername(e.target.value)}
+                                            required
+                                            className='input-area'
+                                        />
+                                    </div>
+                                </label>
+                            </div>
+                        </div>
+                        <div className='pass-container container-lot'>
+                            <div className='password-label'></div>
+                                <label className='email-label'> Password
+                                    <div className='input-lot'>
+                                        <input
+                                            type='password'
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            required
+                                            className='input-area'
+                                        />
                                 </div>
                             </label>
                         </div>
-                    </div>
-                    <div className='email-conmtainer container-lot'>
-                        <div className='email-label'>
-                            <label className='email-label'> Last Name
-                                <div className='input-lot'>
-                                    <input
-                                        type='text'
-                                        value={lastName}
-                                        onChange={(e) => setLastName(e.target.value)}
-                                        required
-                                        className='input-area'
-                                    />
-                                </div>
-                            </label>
-                        </div>
-                    </div>
-                    <div className='email-conmtainer container-lot'>
-                        <div className='email-label'>
-                            <label className='email-label'> Email
-                                <div className='input-lot'>
-                                    <input
-                                        type='email'
-                                        value={email}
-                                        onChange={(e) => setEmail(e.target.value)}
-                                        required
-                                        className='input-area'
-                                    />
-                                </div>
-                            </label>
-                        </div>
-                    </div>
-                    <div className='username-container container-lot'>
-                        <div className='username-label'>
-                            <label className='email-label'> Username
-                                <div className='input-lot'>
-                                    <input
-                                        type='text'
-                                        value={username}
-                                        onChange={(e) => setUsername(e.target.value)}
-                                        required
-                                        className='input-area'
-                                    />
-                                </div>
-                            </label>
-                        </div>
-                    </div>
-                    <div className='pass-container container-lot'>
-                        <div className='password-label'></div>
-                            <label className='email-label'> Password
+                        <div className='confirmPass-container container-lot'>
+                            <label className='email-label'> Confirm Password
                                 <div className='input-lot'>
                                     <input
                                         type='password'
-                                        value={password}
-                                        onChange={(e) => setPassword(e.target.value)}
+                                        value={confirmPassword}
+                                        onChange={(e) => setConfirmPassword(e.target.value)}
                                         required
                                         className='input-area'
                                     />
-                            </div>
-                        </label>
+                                </div>
+                            </label>
+                        </div>
+                        <ul className='errors'>
+                            {errors.map((err, idx) => (
+                                <li className='error-msg' key={idx}> 👎 - - {err}</li>
+                            ))}
+                        </ul>
+                        <div className='button-container'>
+                            <button className='submit-button' type='submit'>Sign up</button>
+                        </div>
                     </div>
-                    <div className='confirmPass-container container-lot'>
-                        <label className='email-label'> Confirm Password
-                            <div className='input-lot'>
-                                <input
-                                    type='password'
-                                    value={confirmPassword}
-                                    onChange={(e) => setConfirmPassword(e.target.value)}
-                                    required
-                                    className='input-area'
-                                />
-                            </div>
-                        </label>
-                    </div>
-                    <div className='button-container'>
-                        <button className='submit-button' type='submit'>Sign up</button>
-                    </div>
-                    <ul className='errors'>
-                        {errors.map((err, idx) => (
-                            <li className='error-msg' key={idx}> 👎 - - {err}</li>
-                        ))}
-                    </ul>
                 </div>
-            </div>
-        </form>
+            </form>
+        </div>
     );
 }
