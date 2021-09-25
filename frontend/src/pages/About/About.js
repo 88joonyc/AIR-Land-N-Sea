54c9d118c636c120f96776d5319a1fac975e254f
88joonyc <joonyc88@gmail.com>
2021-09-25 16:51:49 -0400

    hello

--- frontend/src/pages/About/About.js
+++ frontend/src/pages/About/About.js
@@ -8,7 +8,16 @@ export default function About(){
 
         <>
             <div className="about-me-container">
+                <div className='about-me-intro'>
+                    <h1 className='about-title'>About</h1>
 
+                    <div className='about-ending'>
+                        <p className='about-para'>hello my name is </p>
+                        <p className='about-para'>hello my name is </p>
+                    </div>
+                    <p>hello my name is </p>
+                    <p>hello my name is </p>
+                </div>
 
             </div>
 
